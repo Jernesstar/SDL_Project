f836aaaeae1f3f75b3ecab4a842a40094e598573
Jercy Mukala <jercymukala.gmail.com>
2022-06-19 13:51:58 -0400

    Successfully setup SDL with the project.

---
+++ src/compile.bat
@@ -0,0 +1,1 @@
+g++ src/*.cpp -o src/main -Ipackages/include -Ipackages/include/SDL2 -Lpackages/lib -lmingw32 -lSDL2main -lSDL2 
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include <SDL.h>
+
+int main(int argc, char** argv)
+{
+    std::cout << "Is this working?";
+    return 0;
+}
