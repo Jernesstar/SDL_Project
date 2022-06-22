e9e3340215c00d28f1a40a1b5387de77605cf671
Jercy Mukala <jercymukala.gmail.com>
2022-06-21 20:24:58 -0400

    Added the mixer library, to handle sound. Figured out how to show images on the screen.

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
+
 #include <SDL.h>
 #include <SDL_ttf.h>
+#include <SDL_image.h>
 
-#define SCREEN_WIDTH 1028
-#define SCREEN_HEIGHT 512
+#define SCREEN_WIDTH 1700
+#define SCREEN_HEIGHT 600
 
 int main(int argc, char** argv)
 {
     SDL_Init(SDL_INIT_EVERYTHING);
     TTF_Init();
+    IMG_Init(IMG_INIT_PNG);
 
     // Create window, centered in the middle of screen, 500 x 500, resizable
     SDL_Window* window = SDL_CreateWindow("Game window", 
@@ -17,15 +20,19 @@ int main(int argc, char** argv)
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
 
     TTF_Font* pixel_font = TTF_OpenFont("resources/pixel_font.ttf", 15);
-    SDL_Color color = {0, 255, 0};
+    SDL_Color color = {255, 255, 255};
     SDL_Surface* message_surface = TTF_RenderText_Solid(pixel_font, "Welcome to the window!", color);
     SDL_Texture* message_texture = SDL_CreateTextureFromSurface(renderer, message_surface);
 
-    SDL_Rect message_rect;
-    message_rect.w = 300;
-    message_rect.h = 40;
-    message_rect.x = 128;
-    message_rect.y = 256;
+    SDL_Surface* image = IMG_Load("resources/start_bg.png");
+    SDL_Texture* image_texture = SDL_CreateTextureFromSurface(renderer, image);
+
+    SDL_Rect message_rect = { 128, 256, 500, 40 };
+    SDL_Rect image_rect = { 0, 0, 1700, 600 };
+    // message_rect.w = 500;
+    // message_rect.h = 40;
+    // message_rect.x = 128;
+    // message_rect.y = 256;
 
     SDL_Event event;
     bool running = true;
@@ -40,18 +47,19 @@ int main(int argc, char** argv)
                     break;
             }
         }
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
-        SDL_RenderClear(renderer);
-
+        SDL_RenderCopy(renderer, image_texture, NULL, &image_rect);
         SDL_RenderCopy(renderer, message_texture, NULL, &message_rect);
         SDL_RenderPresent(renderer);
     }
 
     SDL_FreeSurface(message_surface);
+    SDL_FreeSurface(image);
     SDL_DestroyTexture(message_texture);
+    SDL_DestroyTexture(image_texture);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
 
+    IMG_Quit();
     TTF_Quit();
     SDL_Quit();
 
