4791c6479fa50d4103ccbd05f1d16c778deae052
Jercy Mukala <jercymukala.gmail.com>
2022-07-07 14:32:42 -0400

    compile.bat now show how long compiling took.

--- bin/compile.bat
+++ bin/compile.bat
@@ -1,11 +1,15 @@
 @echo off
+
 cls
 echo Compiling...
-MinGW32-make -f bin\Makefile
+
+@REM Will cd into the parent directory of this compile.bat file, represented by %~dp0
+@REM @REM Will run the Makefile, which will compile the project
+for /F "usebackq delims=" %%r in (
+    `powershell -Command "cd '%~dp0'; cd ..; $compiling_time = Measure-Command { MinGW32-make -f bin\Makefile }; $compiling_time.TotalSeconds"`
+) do set "compiling_time=%%r"
+
 if %ERRORLEVEL% == 0 (
-    echo Compiled successfully
-    echo.
-) else (
-    echo An error occurred. Could not build
+    echo Compiling took %compiling_time% seconds
     echo.
 )
