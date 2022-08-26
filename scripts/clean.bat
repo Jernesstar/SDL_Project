850e9abf232c6491f16543c79075e45f1d651853
RUTH NTSHILA <jercymukala@gmail.com>
2022-08-26 07:54:56 -0400

    Updated scripts, renamed compile.bat to build.bat

--- scripts/clean.bat
+++ scripts/clean.bat
@@ -6,6 +6,9 @@ echo Cleaning project
 if exist obj\ (
     rmdir /s /q obj
 )
+if exist bin\ (
+    rmdir /s /q bin
+)
 if exist Makefile  (
     del /q Makefile
 )
@@ -14,15 +17,6 @@ if exist Saddle\Makefile (
 )
 if exist Sandbox\Makefile (
     del /q Sandbox\Makefile
-)
-if exist Saddle\vendor\SDL2\bin\Saddle.lib (
-    del /q Saddle\vendor\SDL2\bin\Saddle.lib
-)
-if exist Saddle\vendor\SDL2\bin\Saddle.dll (
-    del /q Saddle\vendor\SDL2\bin\Saddle.dll
-)
-if exist Saddle\vendor\SDL2\bin\Sandbox.exe (
-    del /q Saddle\vendor\SDL2\bin\Sandbox.exe
 )
 
 echo Project clean
--- scripts/run_project.bat
+++ scripts/run_project.bat
@@ -3,9 +3,9 @@ pushd %~dp0\..\
 
 if exist .\Saddle\vendor\SDL2\bin\Sandbox.exe (
     echo Running project
-    .\Saddle\vendor\SDL2\bin\Sandbox.exe
+    start .\Saddle\vendor\SDL2\bin\Sandbox.exe
 ) else (
-    echo Cannot find Sandbox.exe. Make sure to call compile.bat before calling run_project.bat
+    echo Cannot find Sandbox.exe. Make sure to call build.bat before calling run_project.bat
 )
 
 popd
