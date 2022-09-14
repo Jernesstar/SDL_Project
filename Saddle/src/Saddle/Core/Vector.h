f61a8691f7952ca38ac4d8a1e41f0f0964d4d5d3
RUTH NTSHILA <jercymukala@gmail.com>
2022-09-14 17:56:56 -0400

    feature: Working on refactoring Vector classes

--- Saddle/src/Saddle/Core/Vector.h
+++ Saddle/src/Saddle/Core/Vector.h
@@ -10,22 +10,10 @@ struct Vector2D {
 
     Vector2D(float x = 0, float y = 0) : x(x), y(y) { }
 
-    Vector2D operator +(const Vector2D& other)
-    {
-        return {x + other.x, y + other.y};
-    }
-
     void operator +=(const Vector2D& other)
     {
         auto vec = *this + other;
         x = vec.x; y = vec.y;
-    }
-
-    // Subtraction here
-
-    Vector2D operator *(float scalar)
-    {
-        return {x * scalar, y * scalar};
     }
 
     void operator *=(float scalar)
@@ -33,32 +21,12 @@ struct Vector2D {
         auto vec = *this * scalar;
         x = vec.x; y = vec.y;
     }
-
-    float operator *(const Vector2D& other)
-    {
-        return (x * other.x) + (y * other.y); 
-    }
-
-    float Magnitude()
-    {
-        return sqrtf((*this) * (*this));
-    }
 };
-
-inline std::ostream& operator <<(std::ostream& os, const Vector2D& vec2)
-{
-    return os << "{ " << vec2.x << ", " << vec2.y << " }";
-}
 
 struct Vector3D {
     float x, y, z;
 
     Vector3D(float x = 0, float y = 0, float z = 0) : x(x), y(y), z(z) { }
-
-    Vector3D operator +(const Vector3D& other)
-    {
-        return {x + other.x, y + other.y, z + other.z};
-    }
 
     void operator +=(const Vector3D& other)
     {
@@ -66,29 +34,59 @@ struct Vector3D {
         x = vec.x; y = vec.y; z = vec.z;
     }
 
-    // Subtraction here
-
-    Vector3D operator *(float scalar)
-    {
-        return {x * scalar, y * scalar, z * scalar};
-    }
-
     void operator *=(float scalar)
     {
         auto vec = *this * scalar;
         x = vec.x; y = vec.y; z = vec.z;
     }
-
-    float operator *(const Vector3D& other)
-    {
-        return (x * other.x) + (y * other.y) + (z * other.x); 
-    }
-
-    float Magnitude()
-    {
-        return sqrtf((*this) * (*this));
-    }
 };
+
+// Vector2D functions
+
+inline Vector2D operator +(const Vector2D& vec, const Vector2D& vec2)
+{
+    return {vec.x + vec2.x, vec.y + vec2.y};
+}
+
+// Subtraction here
+
+inline Vector2D operator *(const Vector2D& vec, float scalar)
+{
+    return {vec.x * scalar, vec.y * scalar};
+}
+
+inline float operator *(const Vector2D& vec, const Vector2D& vec2)
+{
+    return (vec.x * vec2.x) + (vec.y * vec2.y); 
+}
+
+inline float Magnitude(const Vector2D& vec) { return sqrtf(vec * vec); }
+
+inline std::ostream& operator <<(std::ostream& os, const Vector2D& vec2)
+{
+    return os << "{ " << vec2.x << ", " << vec2.y << " }";
+}
+
+// Vector3D functions
+
+inline Vector3D operator +(const Vector3D& vec, const Vector3D& vec2)
+{
+    return {vec.x + vec2.x, vec.y + vec2.y, vec.z + vec2.z};
+}
+
+// Subtraction here
+
+inline Vector3D operator *(const Vector3D& vec, float scalar)
+{
+    return {vec.x * scalar, vec.y * scalar, vec.z * scalar};
+}
+
+inline float operator *(const Vector3D& vec, const Vector3D& vec2)
+{
+    return (vec.x * vec2.x) + (vec.y * vec2.y) + (vec.z * vec2.x); 
+}
+
+inline float Magnitude(const Vector3D& vec) { return sqrtf(vec * vec); }
 
 inline std::ostream& operator <<(std::ostream& os, const Vector3D& vec3)
 {
