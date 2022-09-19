85dfea68ca9d7e2e5d21fb30dbaf6a7252c9df5b
RUTH NTSHILA <jercymukala@gmail.com>
2022-09-19 06:41:52 -0400

    feature: Wrote a method to read shader files in.

--- Saddle/src/OpenGL/Shader.cpp
+++ Saddle/src/OpenGL/Shader.cpp
@@ -1,6 +1,10 @@
 #include "Shader.h"
 
+#include <fstream>
+
 #include <glad/glad.h>
+
+#include "Saddle/Core/Assert.h"
 
 namespace Saddle {
 
@@ -19,6 +23,27 @@ Shader::Shader(const std::string& file_path, ShaderType type)
 Shader::~Shader()
 {
     
+}
+
+std::string Shader::Load(const std::string& file_path)
+{
+    std::string result;
+    std::ifstream in(file_path, std::ios::binary | std::ios::in);
+
+    SADDLE_CORE_ASSERT(in, "Could not open file: " + file_path)
+
+    in.seekg(0, std::ios::end); // Sets the read position at the end of the file;
+    // Gets current read position, since read position is at the end of the file
+    // this operation returns the size of the file
+    size_t size = in.tellg();
+
+    SADDLE_CORE_ASSERT(size != -1, "Could not read from file: " + file_path);
+
+    result.resize(size); // Resize the resulting string
+    in.seekg(0, std::ios::beg); // Set the read position to the beginning of the file
+    in.read(result.data(), size); // Read the entire file and output the contents to the string
+
+    return result;
 }
 
 }
