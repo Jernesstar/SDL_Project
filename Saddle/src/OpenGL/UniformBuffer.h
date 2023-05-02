d14717908bc42c8f212aa1411dcda8cb605ce6cc
Jercy Mukala <jercymukala@gmail.com>
2023-05-02 12:33:46 -0400

    progress: Uniform buffers are still not working

--- Saddle/src/OpenGL/UniformBuffer.h
+++ Saddle/src/OpenGL/UniformBuffer.h
@@ -17,14 +17,17 @@ public:
         : Binding(binding)
     {
         glCreateBuffers(1, &m_BufferID);
-        glNamedBufferData(m_BufferID, size, nullptr, dynamic ? GL_DYNAMIC_DRAW : GL_STATIC_DRAW);
+        glBindBuffer(GL_UNIFORM_BUFFER, m_BufferID);
+        glBufferData(GL_UNIFORM_BUFFER, size, nullptr, dynamic ? GL_DYNAMIC_DRAW : GL_STATIC_DRAW);
+
         glBindBufferBase(GL_UNIFORM_BUFFER, binding, m_BufferID);
     }
     ~UniformBuffer() { glDeleteBuffers(1, &m_BufferID); }
 
-    void SetData(uint32_t offset, uint32_t size, const void* data)
+    void SetData(const void* data, uint32_t offset, uint32_t size)
     {
-        glNamedBufferSubData(m_BufferID, offset, size, data);
+        glBindBuffer(GL_UNIFORM_BUFFER, m_BufferID);
+        glBufferSubData(GL_UNIFORM_BUFFER, offset, size, data); 
     }
 
 private:
