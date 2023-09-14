035cb7875d15c535802f97e2e9aa5e2219aa9335
Jercy Mukala <jercymukala@gmail.com>
2023-09-14 11:01:01 -0400

    todo: Get the Text to render correctly

--- Sandbox/src/Demos/FontDemo.h
+++ Sandbox/src/Demos/FontDemo.h
@@ -24,13 +24,13 @@ public:
 
 private:
     Font font1{ "Sandbox/assets/fonts/pixel_font.ttf", 0, 48 };
-    Font font2{ "C:/Users/ruthj/AppData/Local/Microsoft/Windows/Fonts/JetBrainsMono-ExtraBoldItalic.ttf", 0, 48 };
+    Font font2{ "C:/Users/26mukala506_lewiston/AppData/Local/Microsoft/Windows/Fonts/JetBrainsMono-ExtraBoldItalic.ttf", 0, 48 };
     Text text1{ "This is not a piece of sample text", font1, glm::vec4(0.6f, 0.7f, 0.8f, 1.0f) };
     Text text2{ "Jersey, cool name", font2, glm::vec4(0.6f, 0.7f, 0.8f, 1.0f) };
 
     OrthographicCamera camera{ 0.0, 1600.0f, 0.0f, 900.0f };
-    StereographicCamera camera2{ 90.0f, 0.0f, 1.0f, 1600, 900 };
-    CameraController controller{ camera2 };
+    // StereographicCamera camera{ 90.0f, 0.1f, 100.0f, 1600, 900 };
+    CameraController controller{ camera };
 };
 
 FontDemo::FontDemo()
@@ -42,7 +42,7 @@ FontDemo::FontDemo()
             Application::Close();
     });
 
-    camera2.SetPosition({ 0.0f, 0.0f, 500.0f });
+    camera.SetPosition({ 0.0f, 0.0f, 0.0f });
     controller.TranslationSpeed = 0.5f;
 }
 
@@ -51,7 +51,7 @@ void FontDemo::OnUpdate(TimeStep ts)
     controller.OnUpdate(ts);
 
     Renderer::Clear({ });
-    Renderer2D::BeginScene(camera2);
+    Renderer2D::BeginScene(camera);
     {
         Renderer2D::DrawQuad(glm::vec4(1.0f, 1.0f, 1.0f, 1.0f), glm::vec3(200.0f, 300.0f, 0.0f), glm::vec2(50.0f, 50.0f));
         Renderer2D::DrawText(text1, glm::vec3(500.0f, 400.0f, 0.0f));
