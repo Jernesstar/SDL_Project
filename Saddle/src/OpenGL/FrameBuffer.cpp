00e2f22e6ed8c33b9063f90872e760b39bc251f6
Jernesstar <98028191+Jernesstar@users.noreply.github.com>
2023-12-31 05:12:04 +0000

    progress: Finished working on framebuffers mostly, will now figure out how to render using them.

--- Saddle/src/OpenGL/FrameBuffer.cpp
+++ Saddle/src/OpenGL/FrameBuffer.cpp
@@ -3,27 +3,59 @@
 namespace Saddle
 {
 
-FrameBuffer::FrameBuffer(AttachmentType type, int w, int h)
+FrameBuffer::FrameBuffer(AttachmentSpecification specs)
 {
-        glGenFramebuffers(1, &m_BufferID);
+    glGenFramebuffers(1, &m_BufferID);
+    glGenTextures(1, &m_TextureID);
+    glGenRenderbuffers(1, &m_RenderBufferID);
 
-        switch(type)
-        {
-            case AttachmentType::Texture:
-                uint32_t texture;
-                glGenTextures(1, &texture);
-                glBindTexture(GL_TEXTURE_2D, texture);
+    switch(specs.Color)
+    {
+        case AttachmentType::Texture:
+            glBindTexture(GL_TEXTURE_2D, m_TextureID);
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, specs.Width, specs.Height, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+            glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, m_TextureID, 0);
+            break;
 
-                glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, w, h, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+        case AttachmentType::RenderBuffer:
+            break;
+    }
 
-                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-                glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, texture, 0);  
-                break;
+    if(specs.Depth == AttachmentType::RenderBuffer && specs.Stencil == AttachmentType::RenderBuffer)
+    {
+        glBindRenderbuffer(GL_RENDERBUFFER, m_RenderBufferID);
+        glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, specs.Width, specs.Height);
+        glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, m_RenderBufferID);
+    }
 
-            case AttachmentType::RenderBuffer:
-                break;
-        }
+    switch(specs.Depth)
+    {
+        case AttachmentType::Texture:
+            glBindTexture(GL_TEXTURE_2D, m_TextureID);
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, specs.Width, specs.Height, 0, GL_DEPTH_COMPONENT, GL_FLOAT, NULL);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+            break;
+        
+        case AttachmentType::RenderBuffer:
+            break;
+    }
+
+    switch(specs.Stencil)
+    {
+        
+    }
+}
+
+FrameBuffer::~FrameBuffer()
+{
+    glDeleteTextures(1, &m_TextureID);
+    glDeleteRenderbuffers(1, &m_RenderBufferID);
+    glDeleteBuffers(1, &m_BufferID);
 }
 
 }
--- Saddle/src/OpenGL/FrameBuffer.h
+++ Saddle/src/OpenGL/FrameBuffer.h
@@ -8,18 +8,32 @@
 
 namespace Saddle {
 
-enum class AttachmentType { Texture, RenderBuffer };
-
 class FrameBuffer {
+private:
+    enum class AttachmentType { Texture, RenderBuffer };
+
+    struct AttachmentSpecification {
+        uint32_t Width, Height;
+        AttachmentType Color, Depth, Stencil;
+
+        AttachmentSpecification(uint32_t width, uint32_t height,
+            AttachmentType color = AttachmentType::Texture,
+            AttachmentType depth = AttachmentType::RenderBuffer,
+            AttachmentType stencil = AttachmentType::RenderBuffer
+        ) : Width(width), Height(height), Color(color), Depth(depth), Stencil(stencil) { }
+    };
+
 public:
-    FrameBuffer(AttachmentType type, int width, int height);
-    ~FrameBuffer() { glDeleteBuffers(1, &m_BufferID); }
+    FrameBuffer(AttachmentSpecification specs);
+    ~FrameBuffer();
 
     void Bind() { glBindFramebuffer(GL_FRAMEBUFFER, m_BufferID); }
     void Unbind() { glBindFramebuffer(GL_FRAMEBUFFER, 0); }
 
 private:
     uint32_t m_BufferID;
+    uint32_t m_TextureID;
+    uint32_t m_RenderBufferID;
 };
 
 }
